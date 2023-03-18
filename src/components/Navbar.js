9b612c32c6dc9094c734f664e872b49356e8d73b
mankaa <mankaache10@gmail.com>
2023-03-18 05:34:48 +0100

    added a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">About</a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
