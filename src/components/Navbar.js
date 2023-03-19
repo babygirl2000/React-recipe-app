14bebd3be4c2fafa5acb09e13433bc9b58c8747e
mankaa <mankaache10@gmail.com>
2023-03-19 02:56:20 +0100

    made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [showSidebar, setShowSidebar] = useState(false)
+
+
     return ( 
         <div className="navbar container">
             <a href="#!" className="logo">F<span>oo</span>diesHub</a>
@@ -6,6 +11,16 @@ const Navbar = () => {
                 <a href="#!">Home</a>
                 <a href="#!">Recipes</a>
                 <a href="#!">About</a>
+            </div>
+
+            <div 
+            onClick={()=> setShowSidebar(!showSidebar)} 
+            className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+            >
+                <div className="bar"></div> 
+                <div className="bar"></div>
+                <div className="bar"></div>
+
             </div>
         </div>
      );
