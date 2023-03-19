d19630b094d34890cf3120b3a63bdd7b6c0de36a
mankaa <mankaache10@gmail.com>
2023-03-19 22:56:44 +0100

    wip

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 const Recipes = () => {
-    return ( 
-        <div>
-            Recipes
-        </div>
-     );
-}
- 
+  const searches = [
+    "pizza",
+    "burger",
+    "cookies",
+    "juice",
+    "biriyani",
+    "salad",
+    "ice cream",
+    "lasagne",
+    "pudding",
+    "soup",
+  ];
+
+  return (
+    <div className="previous-searches section">
+      <h2>Previous searches</h2>
+      <div className="previous-searches-container">
+        {searches.map((item,index) =>(
+            <div key={index} style={{animationDelay: index * 0.1 + "s"}} className="search-item">{item}</div>
+        ) )}
+      </div>
+      <div className="search-box">
+        <input type="text" placeholder="search..." />
+        <button className="btn">
+            <FontAwesomeIcon icon={faSearch}/>
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default Recipes;
